7572456b944f9645d5296f50b85015da540baaf8
troyeguo <guo362429@gmail.com>
2021-08-07 17:37:15 +0800

    fix bug

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 declare module 'react-native-webdav' {
 
   export default class RNWebdav {
-    list: any
-    createDirectory: any
-    download: any
-    upload: any
+    list: (string:username, string:password, string:url)=>Promise<string>
+    createDirectory: (string:username, string:password, string:url)=>Promise<boolean>
+    download: (string:username, string:password, string:url, string: dest)=>Promise<boolean>
+    upload: (string:username, string:password, string:url, string: source)=>Promise<boolean>
   }
 }
